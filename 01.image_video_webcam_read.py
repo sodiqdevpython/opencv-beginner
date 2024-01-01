39268d3afb1440a64daee6fef77db16de4c7bdfa
sodiqdevpython <sodiqdevpython@gmail.com>
2024-01-01 12:15:49 +0500

    done with hometasks

--- 01.image_video_webcam_read.py
+++ 01.image_video_webcam_read.py
@@ -1,9 +1,8 @@
-
 def s(a):
     cv2.waitKey(a*1000)
     cv2.destroyAllWindows()
 
-#                     Image Read
+#!                     Image Read
 
 import cv2
 
@@ -15,10 +14,10 @@ cv2.imshow("Loki Image", img)
 
 s(2)
 
-print('Destroyed')
+# print('Destroyed')
 
 
-#                    Video Read
+#!                     Video Read
 
 import cv2
 
@@ -51,5 +50,78 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
 capture.release()
 cv2.destroyAllWindows()
+
+#?  Homework 
+
+# 1.Get image and resize it 
+# 2.Simple video reader 
+# 3.Capture read and resize it
+
+# Task 1  --done
+
+"""
+import cv2
+
+img = cv2.imread('images/mountain.webp', 0)
+
+img = cv2.resize(img, (300,300))
+
+cv2.imshow("winname", img)
+
+cv2.waitKey(0)
+"""
+
+
+# Task 2 
+
+
+import cv2
+
+vid = cv2.VideoCapture('images/loki.jpg')
+h = 200
+w = 200
+
+vid.set(3, w)
+vid.set(4, h)
+
+while True:
+    success, video = vid.read()
+    video = cv2.resize(video, (w,h))
+    cv2.imshow("This is Elon Musk", video)
+    print(success)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
+
+# Task 3
+
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+w = 200
+h = 200
+
+cap.set(3,w)
+cap.set(4,h)
+
+while True:
+    success, video = cap.read()
+    video = cv2.resize(video, (w,h))
+    video = cv2.cvtColor(video, cv2.COLOR_RGB2GRAY)
+    
+    cv2.imshow("winname", video)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
+
